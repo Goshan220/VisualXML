de6022dbfbe33a274eb7fd9385b47de8e1021efb
Igor Smirnov <Goshan2200@gmail.com>
2017-10-10 22:46:02 +0300

    Окно редактора, загрузка и сохранение файлов

--- code/Main.py
+++ code/Main.py
@@ -1,9 +1,55 @@
-def test():
-    for i in range(5):
-        print('test ' + str(i))
-        print("yeah!")
+from tkinter import *
+from tkinter import filedialog
 
 
-test()
+def Quit(ev):
+    global root
+    root.destroy()
 
-print("ooykeeey, it's 3 commit and 2 commit on dev branch")
+def LoadFile(ev):
+    fn =  filedialog.Open(root, filetypes=[('*.xml files', '.xml')]).show()
+    if fn == '':
+        return
+    textbox.delete('1.0', 'end')
+    textbox.insert('1.0', open(fn, 'rt').read())
+
+
+def SaveFile(ev):
+    fn =  filedialog.SaveAs(root, filetypes=[('*.txt files', '.txt')]).show()
+    if fn == '':
+        return
+    if not fn.endswith(".txt"):
+        fn += ".txt"
+    open(fn, 'wt').write(textbox.get('1.0', 'end'))
+
+
+root = Tk()
+
+panelFrame = Frame(root, height=60, bg='gray')
+textFrame = Frame(root, height=340, width=600)
+
+panelFrame.pack(side='top', fill='x')
+textFrame.pack(side='bottom', fill='both', expand=1)
+
+textbox = Text(textFrame, font='Fristyle 14', wrap='word')
+scrollbar = Scrollbar(textFrame)
+
+scrollbar['command'] = textbox.yview
+textbox['yscrollcommand'] = scrollbar.set
+
+textbox.pack(side='left', fill='both', expand=1)
+scrollbar.pack(side='right', fill='y')
+
+loadBtn = Button(panelFrame, text='Load')
+saveBtn = Button(panelFrame, text='Save')
+quitBtn = Button(panelFrame, text='Quit')
+
+loadBtn.bind("<Button-1>", LoadFile)
+saveBtn.bind("<Button-1>", SaveFile)
+quitBtn.bind("<Button-1>", Quit)
+
+loadBtn.place(x=10, y=10, width=40, height=40)
+saveBtn.place(x=60, y=10, width=40, height=40)
+quitBtn.place(x=110, y=10, width=40, height=40)
+
+root.mainloop()
