82119cac2ac50f6d3a28fe9a8241c70b17e67896
Igor <goshan2200@gmail.com>
2018-03-30 17:16:20 +0300

    Refactor to PEP

--- code/Main.py
+++ code/Main.py
@@ -2,32 +2,34 @@ from tkinter import *
 from tkinter import filedialog
 from Parser  import Pars
 
+
 class App:
     def __init__(self):
         self.listbox_item = {}
-        self.StartGUI()
+        self.start_GUI()
 
-    def Help(self, ev):
+    def help(self, ev):
         self.textbox.delete('1.0', 'end')
         self.textbox.insert('1.0', open("~help.txt", 'rt', encoding='utf-8').read())
 
-    def LoadFile(self, ev):
+    def load_file(self, ev):
         self.listbox.delete(0, END)
 
-        fn = filedialog.Open(self.root, filetypes = [('*.xml files', '.xml')]).show()
+        fn = filedialog.Open(self.root, filetypes=[('*.xml files', '.xml')]).show()
         if fn == '':
             return
         text = open(fn)
         pars = Pars()
         pars.start(text)
-        self.listbox_item = pars.getTagDict()
+        self.listbox_item = pars.get_tag_dict()
+        # self.listbox_item = pars.getTextDict()
 
         self.textbox.delete('1.0', 'end')
         self.textbox.insert('1.0', open("~temp.txt", 'rt', encoding='utf-8').read())
         for item in self.listbox_item:
             self.listbox.insert(END, item)
 
-    def ShowFile(self, ev):
+    def show_file(self, ev):
         self.listbox.delete(0, END)
         self.textbox.delete('1.0', 'end')
         self.textbox.insert('1.0', open("~temp.txt", 'rt', encoding='utf-8').read())
@@ -35,8 +37,7 @@ class App:
         for item in self.listbox_item:
             self.listbox.insert(END, item)
 
-
-    def SaveTag(self, ev):
+    def save_tag(self, ev):
         fn = filedialog.SaveAs(self.root, filetypes=[('*.txt files', '.txt')]).show()
         if fn == '':
             return
@@ -44,7 +45,7 @@ class App:
             fn += ".txt"
         open(fn, 'wt').write(self.textbox.get('1.0', 'end'))
 
-    def StartGUI(self):
+    def start_GUI(self):
         self.root = Tk()
         self.root.title("VisualXML")
         self.root.geometry("550x500")
@@ -53,7 +54,7 @@ class App:
         self.panelFrame = Frame(self.root, bg='gray', height=105)
         self.textFrame = Frame(self.root)
         self.panelFrame.pack(side='top', fill='x')
-        self.textFrame.pack(side='bottom', fill='both' , expand=1)
+        self.textFrame.pack(side='bottom', fill='both', expand=1)
 
         self.textbox = Text(self.textFrame, font='Fristyle 10', wrap='word')
         self.scrollbar = Scrollbar(self.textFrame)
@@ -78,18 +79,18 @@ class App:
 
         self.listbox.place(x=230, y=10)
 
-        loadBtn = Button(self.panelFrame, text='Load XML File')
-        helpBtn = Button(self.panelFrame, text='Help')
-        showFile = Button(self.panelFrame, text='Show file')
-        saveFile = Button(self.panelFrame, text='Save tag into file')
-        loadBtn.bind("<Button-1>", self.LoadFile)
-        helpBtn.bind("<Button-1>", self.Help)
-        showFile.bind("<Button-1>", self.ShowFile)
-        saveFile.bind("<Button-1>", self.SaveTag)
-        loadBtn.place(x=10,  y=10, width=100, height=35)
-        helpBtn.place(x=120, y=10, width=100, height=35)
-        showFile.place(x=10, y=55, width=100, height=35)
-        saveFile.place(x=120, y=55, width=100, height=35)
+        load_btn = Button(self.panelFrame, text='Load XML File')
+        help_btn = Button(self.panelFrame, text='Help')
+        show_file = Button(self.panelFrame, text='Show file')
+        save_file = Button(self.panelFrame, text='Save tag into file')
+        load_btn.bind("<Button-1>", self.load_file)
+        help_btn.bind("<Button-1>", self.help)
+        show_file.bind("<Button-1>", self.show_file)
+        save_file.bind("<Button-1>", self.save_tag)
+        load_btn.place(x=10,  y=10, width=100, height=35)
+        help_btn.place(x=120, y=10, width=100, height=35)
+        show_file.place(x=10, y=55, width=100, height=35)
+        save_file.place(x=120, y=55, width=100, height=35)
 
         self.root.rowconfigure(0, weight=1)
         self.root.columnconfigure(0, weight=1)
